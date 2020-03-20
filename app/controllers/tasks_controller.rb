fb3b760dded0925ff4366f236b2eb061f18ee9b2
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2020-03-20 01:52:50 +0000

    2回目のコミット

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,31 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
+  def index
+     @tasks = Task.all
+  end
   
   def new
     @task = Task.new
   end
   
   def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "新しいタスクを作成しました。"
     redirect_to tasks_index_url
+    else
+      render :new
+    end  
   end
   
-  def index
-    @task = Task.all
-  end
+  private
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+    
+    def task_params
+      params.require(:task).permit(:name,:description,:user_id)
+    end  
 end
