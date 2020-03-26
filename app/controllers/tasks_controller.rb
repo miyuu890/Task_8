7a55c4afe84c8e671ec3536bfdeaed206688189a
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2020-03-26 12:46:25 +0000

    バリデーション前

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,23 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+    
+       flash[:success] = "タスクを更新しました。"
+       redirect_to user_task_url(current_user) 
+    else
+      render :edit
+    end  
+  end  
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = "タスクを削除しました。"
+    redirect_to user_tasks_url(current_user)
+  end  
   
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'tasks/index', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  post 'tasks/:id/update',to:'posts#update', as: :update_task
+  post 'tasks/:id/destroy',to: 'tasks#destroy', as: :destroy_task
 
   root 'static_pages#top'
   get '/signup', to: 'users#new'
