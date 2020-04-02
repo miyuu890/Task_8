512ab4b89511947c78c3afa58abd8405df9025ce
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2020-04-02 14:39:31 +0000

    finish d

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,26 @@
 # coding: utf-8
 
-User.create!( name: "Sample User",
-              email: "sample@email.com",
-              password: "password",
-              password_confirmation: "password")
+User.create!(name: "Sample User",
+             email: "sample@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+users = User.order(:created_at).take(3)
+50.times do |n|
+  name =Faker::Lorem.sentence(3)
+  description = Faker::Lorem.sentence(5)
+  users.each { |user| user.tasks.create!(name: name, 
+  description: description) }
+
+end
